fab7c916ff40ded4f03cb15572efccd6116736a9
Alexander <ashuaibu@getcarbon.co>
2019-10-26 20:51:20 +0100

    interface fixes

---
+++ build/precache-manifest.4421d65963ea1fa70a8493a39856b053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdeca79897a15a31e71b92d49c9a6eab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d7849b398d65084fe46",
+    "url": "/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "7caf1f26e8a986d2fe4d",
+    "url": "/static/css/main.2a4cdc8b.chunk.css"
+  },
+  {
+    "revision": "5d7849b398d65084fe46",
+    "url": "/static/js/2.546ebecc.chunk.js"
+  },
+  {
+    "revision": "7caf1f26e8a986d2fe4d",
+    "url": "/static/js/main.be46a062.chunk.js"
+  },
+  {
+    "revision": "943954cf1819992c3255",
+    "url": "/static/js/runtime-main.429084ec.js"
+  }
+]);
